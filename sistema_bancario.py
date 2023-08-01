c1279ab70b3fb27f0b62f70a42b2317f94e9c02d
cesargodoi <cesar.godoi@gmail.com>
2023-08-01 05:38:48 -0300

    feat: Depósito - verifica se é número - aceita float com ',' ou '.' - só aceita valores positivos - grava o saldo em float - retorna o valor do saldo atual

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,14 +1,17 @@
+import re
+
+
 menu = """
 
-[d] Depositar
+[D] Depositar
 [s] Sacar
 [e] Extrato
 [q] Saír
 
 => """
 
-saldo = 0
-limite = 500
+saldo = 0.0
+LIMITE = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
@@ -16,8 +19,26 @@ LIMITE_SAQUES = 3
 while True:
     opcao = input(menu)
 
-    if opcao == "d":
-        print(" Depósito ")
+    if opcao in "Dd":
+        print(" Depósito ".center(40, "-"))
+
+        while True:
+            _valor = input("Entre com o valor do depósito: ")
+            if re.findall("[a-zA-Z]", _valor):
+                print("*** APENAS NÚMEROS ***")
+            else:
+                if "," in _valor:
+                    _valor = _valor.replace(",", ".")
+                if float(_valor) < 0:
+                    print("*** APENAS VALORES POSITIVOS ***")
+                else:
+                    valor = round(float(_valor), 2)
+                    saldo += valor
+                    extrato += f"{' DEP '.ljust(25, '.')} R$ {valor:10.2f}"
+                    break
+
+        print(" Depósito realizado com sucesso! ".center(40, "-"))
+        print(f"{' SALDO ATUAL '.ljust(25, '.')} R$ {saldo:10.2f}")
 
     elif opcao == "s":
         print("Sacar")
