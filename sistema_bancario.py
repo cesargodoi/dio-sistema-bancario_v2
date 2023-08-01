74de7bee2772e5795d34efd3c3e4a4cd9b36c72e
cesargodoi <cesar.godoi@gmail.com>
2023-08-01 07:26:15 -0300

    feat: Saque - faz as validações para depósito - verifica se o saldo está zerado - verifica o limite de saques diários - verifica se valor > limite - verifica se valor > saldo

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -4,14 +4,14 @@ import re
 menu = """
 
 [D] Depositar
-[s] Sacar
+[S] Sacar
 [e] Extrato
 [q] Saír
 
 => """
 
 saldo = 0.0
-LIMITE = 500
+LIMITE = 500.0
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
@@ -25,23 +25,66 @@ while True:
         while True:
             _valor = input("Entre com o valor do depósito: ")
             if re.findall("[a-zA-Z]", _valor):
-                print("*** APENAS NÚMEROS ***")
+                print("*** APENAS NÚMEROS ***".center(40))
             else:
                 if "," in _valor:
                     _valor = _valor.replace(",", ".")
                 if float(_valor) < 0:
-                    print("*** APENAS VALORES POSITIVOS ***")
+                    print("*** APENAS VALORES POSITIVOS ***".center(40))
                 else:
                     valor = round(float(_valor), 2)
                     saldo += valor
-                    extrato += f"{' DEP '.ljust(25, '.')} R$ {valor:10.2f}"
+                    extrato += f"{' DEP '.ljust(24, '.')} R$  {valor:10.2f}"
+                    print(" Depósito realizado com sucesso! ".center(40, "-"))
+                    print(
+                        f"{' SALDO ATUAL '.ljust(24, '.')} R$  {saldo:10.2f}"
+                    )
                     break
 
-        print(" Depósito realizado com sucesso! ".center(40, "-"))
-        print(f"{' SALDO ATUAL '.ljust(25, '.')} R$ {saldo:10.2f}")
+    elif opcao in "Ss":
+        print(" Saque ".center(40, "-"))
 
-    elif opcao == "s":
-        print("Sacar")
+        while True:
+            if saldo == 0.0:
+                print("*** VOCÊ NÃO POSSUI SALDO ***".center(40))
+                break
+            if numero_saques == LIMITE_SAQUES:
+                print("*** LIMITE DIÁRIO DE SAQUES ESGOTAGO ***".center(40))
+                print("*** VOLTE AMANHÃ! ***".center(40))
+                break
+
+            _valor = input("Entre com o valor do saque: ")
+            if re.findall("[a-zA-Z]", _valor):
+                print("*** APENAS NÚMEROS ***".center(40))
+            else:
+                if "," in _valor:
+                    _valor = _valor.replace(",", ".")
+                if float(_valor) < 0:
+                    print("*** APENAS VALORES POSITIVOS ***".center(40))
+                elif float(_valor) > LIMITE:
+                    print(
+                        f"*** LIMITE POR SAQUE: R$ {LIMITE:.2f} ***".center(40)
+                    )
+                elif float(_valor) > saldo:
+                    print("*** SEU SALDO NÃO É SUFICIENTE ***".center(40))
+                    print(
+                        f"{' SALDO ATUAL '.ljust(24, '.')} R$  {saldo:10.2f}"
+                    )
+                else:
+                    numero_saques += 1
+                    valor = round(float(_valor), 2)
+                    saldo -= valor
+                    extrato += f"{' SAQ '.ljust(24, '.')} R$ -{valor:10.2f}"
+                    print(" Saque realizado com sucesso! ".center(40, "-"))
+                    print(
+                        f"{' SALDO ATUAL '.ljust(24, '.')} R$  {saldo:10.2f}"
+                    )
+                    print(
+                        "*** SAQUES RESTANTES: {} ***".format(
+                            LIMITE_SAQUES - numero_saques
+                        ).center(40)
+                    )
+                    break
 
     elif opcao == "e":
         print(" Estrato ")
